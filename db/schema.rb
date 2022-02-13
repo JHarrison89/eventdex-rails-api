0c6e8a3ded9b3c182d7cbe57c14755424d26daa1
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2022-02-13 12:34:26 +0000

    Jobs migration & seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_31_193422) do
+ActiveRecord::Schema.define(version: 2022_02_13_114554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2022_01_31_193422) do
   create_table "jobs", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "source"
+    t.string "employer"
+    t.string "title"
+    t.string "location"
+    t.text "description"
+    t.string "link"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "faker"
+
+puts "Running seed...🌱🌱🌱"
+puts "Destorying tables..."
+Job.destroy_all
+
+puts "Creating jobs..."
+10.times do
+    Job.create(
+        source: Faker::Internet.domain_name,
+        employer: Faker::Company.name,
+        title: Faker::Job.title
+        location: Faker::Address.city,
+        description: Faker::Lorem.paragraphs,
+        link: Faker::Internet.url,
+    )
+end
+
+puts "Seed complete...🌴🌴🌴"
+
+
